cdb68118668a858d99ffd7072ea6c92eb7f1bafd
BeginnerCoder21 <60263821+BeginnerCoder21@users.noreply.github.com>
2021-01-04 19:44:29 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,12 +1,19 @@
 function players()
 {
-  let p1=document.getElementById("p1").value;
-  let p2=document.getElementById("p2").value;
-  alert(p1+" will start with X and \r\n"+p2+" will start with O");
+  var p1=document.getElementById("p1").value;
+  var p2=document.getElementById("p2").value;
+  if((p1==" "||p1=="")||(p2=="" || p2==" "))
+  {
+    alert("Please enter names first !!!");
+  }
+  else {
+    alert(p1+" will start with X and \r\n"+p2+" will start with O");
+    next();
+  }
   //export{p1,p2};
-  next();
 }
 function next()
 {
   window.open("page3.html","_self");
 }
+
